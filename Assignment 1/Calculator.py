5835fe69e5d11ba83d7d2ba315c3af76ed8f23da
Arsalan Khan <114850731+eakbangash@users.noreply.github.com>
2022-10-03 15:58:35 +0500

    Add files via upload

---
+++ Assignment 1/Calculator.py
@@ -0,0 +1,14 @@
+first_number = float(input("Enter 1st Number"))
+second_number = float(input("Enter 2nd Number"))
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+division = first_number / second_number
+reminder = first_number % second_number
+print("The")
+print( "sum is", sum ,"\n")
+print( "difference is", difference ,"\n")
+print("product is", product,"\n")
+print("division is", division, "\n")
+print("reminder is", reminder)
